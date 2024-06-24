0aef45a0456f4865ec7b13f2d16499a47414cb70
milkers69 <127353774+milkers69@users.noreply.github.com>
2024-06-24 15:43:46 -0500

    Only iterate over torrents once

--- app.py
+++ app.py
@@ -13,27 +13,26 @@ qbt_client = qbittorrentapi.Client(host=host, username=username, password=passwo
 # try to login, throws qbittorrentapi.LoginFailed exception
 qbt_client.auth_log_in()
 
-# get torrents
-torrents = qbt_client.torrents_info()
-
 # https://qbittorrent-api.readthedocs.io/en/latest/apidoc/definitions.html#qbittorrentapi.definitions.TrackerStatus
+torrents_to_remove = set()
+torrents_to_add = set()
+# for all torrents
+for torrent in qbt_client.torrents_info():
+    # torrents that are tagged and working now
+    if tag in torrent.tags and any(tracker.status == 2 for tracker in torrent.trackers):
+        torrents_to_remove.add(torrent.hash)
+    # torrents that are not tagged and not working now
+    elif tag not in torrent.tags all(tracker.status in (0, 1, 4) for tracker in torrent.trackers):
+        torrents_to_add.add(torrent.hash)
+    
 # cleanup tagged torrents that are working now
 qbt_client.torrent_tags.remove_tags(
     tags=[tag],
-    torrent_hashes=[
-        torrent.hash
-        for torrent in torrents
-        if tag in torrent.tags
-        and any(tracker.status == 2 for tracker in torrent.trackers)
-    ],
+    torrent_hashes=torrents_to_remove,
 )
 
 # tag torrents that are not working now
 qbt_client.torrent_tags.add_tags(
     tags=[tag],
-    torrent_hashes=[
-        torrent.hash
-        for torrent in torrents
-        if all(tracker.status in (0, 1, 4) for tracker in torrent.trackers)
-    ],
+    torrent_hashes=torrents_to_add,
 )
